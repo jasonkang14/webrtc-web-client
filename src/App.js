b31aa4dcc9fcc994a3608f25a212c58443faa645
Jason Kang <jasonkang14@gmail.com>
2022-09-30 18:57:09 +0900

    Add a way to get local video track

--- src/App.js
+++ src/App.js
@@ -1,22 +1,25 @@
-import logo from './logo.svg';
+import {useEffect, useRef} from 'react';
 import './App.css';
 
 function App() {
+  const localVideoRef = useRef(null);
+
+  const getLocalVideo = async () => {
+    const mediaStream = await navigator.mediaDevices.getUserMedia({video: true})
+    localVideoRef.current.srcObject = mediaStream;
+  }
+
+  useEffect(() => {
+    localVideoRef.current.onloadedmetadata = () => {
+      localVideoRef.current.play();
+    };
+  }, [])
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <video id="localVideo" ref={localVideoRef} />
+        <button  onClick={getLocalVideo}>카메라 가져오기</button>
       </header>
     </div>
   );
